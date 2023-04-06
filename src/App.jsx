cee13d53fd15e0e1a2a08da4165e31fb1e419ad3
tisha-akter <tishaakter.jui79@gmail.com>
2023-04-06 18:12:02 +0600

    created nav toggle and navbar animation

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+
+    <div className="App">
+       <Navbar></Navbar>
+       <h1 className='text-7xl text-purple-600'>Hello from tailwind</h1>
+
+    </div>
+  )
+}
+
+export default App
